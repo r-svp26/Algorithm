e1fbc4b08ca980e94fff9b571ac98a689cbc0889
r-svp26 <riteshk6341@gmail.com>
2021-08-10 00:05:42 +0530

    [Ritesh] Add: ArrayPermutation

---
+++ Algorithm/ArrayPermutation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    public class ArrayPermutation
+    {
+        
+        static int fact = 1;
+        /// <summary>
+        /// iterative method
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int IterativeFactorial(int number)
+        {
+            for (int i=1; i<=number; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+        /// <summary>
+        /// recursive method
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static int RecursiveFactorial(int number)
+        { 
+            if (number == 0 || number == 1)
+                return 1;
+            return number * RecursiveFactorial(number - 1);
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -4,9 +4,43 @@ namespace Algorithm
 {
     class Program
     {
+        /// <summary>
+        /// invoke all the classes from here!
+        /// </summary>
+        static void InvokeClass()
+        {
+            bool CONTINUE = true;
+            while (CONTINUE)
+            {
+                Console.WriteLine("Enter your choice:");
+                Console.WriteLine("1: ArrayPermutation");
+                Console.WriteLine("0: Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        int factIterative = ArrayPermutation.IterativeFactorial(5);
+                        Console.WriteLine("Factorial of a number by iteration method:" + factIterative);
+                        int factRecursive = ArrayPermutation.RecursiveFactorial(5);
+                        Console.WriteLine("Factorial of a number by recursive method:" + factRecursive);
+                        break;
+                    case 0:
+                        Console.WriteLine("Thank you for using Algorithm!");
+                        CONTINUE = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please, Select valid option!");
+                        break;
+                }
+            }
+        }
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Programs.");
+            Program.InvokeClass();
         }
     }
 }
