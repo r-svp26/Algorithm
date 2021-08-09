4174f980a393623599e0978a9c44ae76537dfcf4
r-svp26 <riteshk6341@gmail.com>
2021-08-10 00:36:35 +0530

    [Ritesh] Add: BinarySearchWord

---
+++ Algorithm/BinarySearchWord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    class BinarySearchWord<T> where T : IComparable
+    {
+        T[] array;
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="array"></param>
+        public BinarySearchWord(T[] array)
+        {
+            this.array = array;
+            Array.Sort(this.array);
+        }
+        /// <summary>
+        /// search word
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="element"></param>
+        /// <returns></returns>
+        public int BinarySearch(T[] array, T element)
+        {
+            int low = 0, high = array.Length - 1;
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+                int key = element.CompareTo(array[mid]);
+                if (key == 0)
+                    return mid;
+                if (key > 0)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            return -1;
+        }
+    }
+}
--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Algorithm
 {
@@ -9,11 +10,15 @@ namespace Algorithm
         /// </summary>
         static void InvokeClass()
         {
+            string filepath = @"V:\BridgeLabz\Algorithm\Algorithm\BinarySearch.txt";
+            string text = File.ReadAllText(filepath);
+            string[] textArray = text.Split(" ");
             bool CONTINUE = true;
             while (CONTINUE)
             {
-                Console.WriteLine("Enter your choice:");
+                Console.WriteLine("Enter your choice");
                 Console.WriteLine("1: ArrayPermutation");
+                Console.WriteLine("2: BinarySearchWord");
                 Console.WriteLine("0: Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
@@ -23,6 +28,20 @@ namespace Algorithm
                         Console.WriteLine("Factorial of a number by iteration method:" + factIterative);
                         int factRecursive = ArrayPermutation.RecursiveFactorial(5);
                         Console.WriteLine("Factorial of a number by recursive method:" + factRecursive);
+                        break;
+                    case 2:
+                        BinarySearchWord<string> search = new BinarySearchWord<string>(textArray);
+                        Console.WriteLine("Enter the word to search");
+                        string searchElement = Console.ReadLine();
+                        int found = search.BinarySearch(textArray, searchElement);
+                        if (found == -1)
+                        {
+                            Console.WriteLine("Sorry! Word is not present.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("{0} is present in the file", searchElement);
+                        }
                         break;
                     case 0:
                         Console.WriteLine("Thank you for using Algorithm!");
